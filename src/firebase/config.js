caaca01bb2f897bda95816a94c5955cf9763b2d1
pras75299 <singh.prashantking@gmail.com>
2020-12-21 16:38:52 +0530

    remove not required files and setup firebase for project

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+let firebaseConfig = {
+    apiKey: "AIzaSyBUhDuwSRLjHaWorOmLC82JPOXaY3bKwGk",
+    authDomain: "reactimagegallery-a4230.firebaseapp.com",
+    projectId: "reactimagegallery-a4230",
+    storageBucket: "reactimagegallery-a4230.appspot.com",
+    messagingSenderId: "951983162716",
+    appId: "1:951983162716:web:11c3ff1b45a4717d23ad99"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
