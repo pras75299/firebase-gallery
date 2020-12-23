97f6097d733d32919cb1fd7971aa5f7bea3524bc
pras75299 <singh.prashantking@gmail.com>
2020-12-24 00:34:00 +0530

    update config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 let firebaseConfig = {
-    apiKey: "AIzaSyBUhDuwSRLjHaWorOmLC82JPOXaY3bKwGk",
+    apiKey: `${process.env.local.FIREBASE_API_KEY}`,
     authDomain: "reactimagegallery-a4230.firebaseapp.com",
     projectId: "reactimagegallery-a4230",
     storageBucket: "reactimagegallery-a4230.appspot.com",
     messagingSenderId: "951983162716",
-    appId: "1:951983162716:web:11c3ff1b45a4717d23ad99"
+    appId: `${process.env.local.FIREBASE_APPID}`
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
